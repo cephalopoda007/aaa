102562825ba045b1d75107ca7aad4941b081380f
arek <arek07ty@gmail.com>
2019-06-07 16:39:57 +0200

    Create New Model: Kurs, PozycjaZamowneia, Kategoria, Zamowienie

---
+++ Sklep_Interentowy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sklep_Interentowy.MvcApplication" Language="C#" %>
---
+++ Sklep_Interentowy/Models/Kategoria.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Sklep_Interentowy.Models
+{
+    public class Kategoria
+    {
+        public int KategoriaId { get; set; }
+        public string NazwaKategori { get; set; }
+        public string OpisKategori { get; set; }
+        public string NazwaPlikuIkony { get; set; }
+
+        public virtual ICollection<Kurs> Kursy { get; set; }
+
+    }
+}
